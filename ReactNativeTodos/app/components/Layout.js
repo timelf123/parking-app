ac5878dace4c58393a3429c913b4cfb79bc9d9b8
gerard sychay <hellogerard@gmail.com>
2015-10-28 15:55:53 -0400

    Add parking spot claim/release button logic. Subscribe to parking data, users.

--- ReactNativeTodos/app/components/Layout.js
+++ ReactNativeTodos/app/components/Layout.js
@@ -1,134 +1,238 @@
 let React = require('react-native');
 let {
+  StyleSheet,
   View,
   Text,
   TabBarIOS,
-  NavigatorIOS,
   Navigator,
-  ActionSheetIOS,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback
 } = React;
 
-let ListsContainer = require('./ListsContainer');
+let _ = require('underscore');
+let ddp = require('../config/ddp');
 let SignIn = require('./SignIn');
 let Join = require('./Join');
 
 let Layout = React.createClass({
   displayName: 'Layout',
 
-  propTypes: {
-    userId: React.PropTypes.string,
-    loggedIn: React.PropTypes.bool,
-    changeLogin: React.PropTypes.func
-  },
-
   getInitialState() {
     return {
-      selectedTab: 'public'
+      connected: false,
+      loggedIn: false,
+      userId: null,
+      spots: [],
+      spotsObserver: null,
+      users: [],
+      usersObserver: null
     }
   },
 
-  handleLogout() {
-    this.refs.privateNavigator.push({
-      id: 'signIn'
-    })
-    this.props.changeLogin({loggedIn: false});
-  },
+  getSpots() {
+    if (!this.state.spotsObserver) {
+      ddp.subscribe('spots').then(() => {
+        let spotsObserver = ddp.collections.observe(() => {
+          let spots = [];
+          if (ddp.collections.spots) {
+            spots = ddp.collections.spots.find();
+          }
+          return spots;
+        });
 
-  handleLogin(res) {
-    this.refs.privateNavigator.push({
-      id: 'list',
-      userId: res.userId
-    })
-    this.props.changeLogin(res);
-  },
+        this.setState({spotsObserver: spotsObserver});
 
-  handleMore(isPrivate) {
-    let options = ["Add New List", "Cancel"];
-    if (isPrivate === true) {
-      options.push("Logout");
+        spotsObserver.subscribe((results) => {
+          this.setState({spots: results});
+        });
+      });
     }
+  },
 
-    ActionSheetIOS.showActionSheetWithOptions({
-      options: options,
-      cancelButtonIndex: 1,
-      destructiveButtonIndex: 2,
-    },
-    (buttonIndex) => {
-      if (buttonIndex === 0) {
-        // Add a new list
-        this.props.addNewList(isPrivate);
-      } else if (buttonIndex === 2 && isPrivate) {
-        this.handleLogout({loggedIn: false});
+  getUsers() {
+    if (!this.state.usersObserver) {
+      ddp.subscribe('users').then(() => {
+        let usersObserver = ddp.collections.observe(() => {
+          let users = [];
+          if (ddp.collections.users) {
+            users = ddp.collections.users.find();
+          }
+          return users;
+        });
+
+        this.setState({usersObserver: usersObserver});
+
+        usersObserver.subscribe((results) => {
+          console.log('RES', results);
+          this.setState({users: results});
+        });
+      });
+    }
+  },
+
+  componentWillMount() {
+    ddp.initialize().then(() => {
+      return ddp.loginWithToken();
+    }).then((res) => {
+      let state = {
+        connected: true,
+        loggedIn: false
+      };
+      if (res.loggedIn === true) {
+        state.loggedIn = true;
+        state.userId= res.userId;
       }
+      this.setState(state);
     });
   },
 
-  renderPublicNavigator() {
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+    this.getSpots();
+    this.getUsers();
+  },
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.getSpots();
+    this.getUsers();
+  },
+
+  componentWillUnmount() {
+    if (this.state.spotsObserver) {
+      this.state.spotsObserver.dispose();
+    }
+  },
+
+  handleLogout(res) {
+    this.refs.navigator.pop()
+    ddp.logout();
+    this.setState(res);
+  },
+
+  handleLogin(res) {
+    this.refs.navigator.push({
+      id: 'main',
+      userId: res.userId
+    })
+    this.setState(res);
+  },
+
+  toggleButtonState() {
+    newState = !!!this.getOccupier()
+    if (newState) {
+      ddp.call('Spots.update', [{ name: 'eotw' }, { $set: { user: this.state.userId }}]);
+    }
+    else {
+      ddp.call('Spots.update', [{ name: 'eotw' }, { $set: { user: null }}]);
+    }
+  },
+
+  getOccupier() {
+    let spot = _.findWhere(this.state.spots, { name: 'eotw' });
+    if (spot && spot.user) {
+      let occupier = _.findWhere(this.state.users, { _id: spot.user });
+      if (occupier) {
+        return occupier.emails[0].address
+      }
+    }
+  },
+
+  renderAvailable() {
     return (
-      <NavigatorIOS
-        style={{flex: 1}}
-        initialRoute={{
-          component: ListsContainer,
-          title: 'Public Lists',
-          rightButtonTitle: 'More',
-          onRightButtonPress: () => {
-            this.handleMore(false);
-          }
-        }}
-        />
+      <TouchableOpacity onPress={this.toggleButtonState} activeOpacity={0.85}>
+        <Image style={styles.button} source={require('image!green_button')}>
+          <Text style={styles.buttonText}>CLAIM SPOT</Text>
+        </Image>
+      </TouchableOpacity>
     );
   },
 
-  renderPrivateScene(route, navigator) {
-    if (route.id === 'list') {
-      let userId = this.props.userId;
-
-      if (route.userId) {
-        userId = route.userId;
-      }
-
+  renderOccupied() {
+    let spot = _.findWhere(this.state.spots, { name: 'eotw' });
+    if (spot && spot.user === this.state.userId) {
       return (
-        <NavigatorIOS
-          style={{flex: 1}}
-          initialRoute={{
-            component: ListsContainer,
-            title: 'Private Lists',
-            passProps: { userId: userId },
-            rightButtonTitle: 'More',
-            onRightButtonPress: () => {
-              this.handleMore(true);
-            }
-          }}
-          />
+        <TouchableOpacity onPress={this.toggleButtonState} activeOpacity={0.85}>
+          <Image style={styles.button} source={require('image!red_button')}>
+            <Text style={styles.buttonText}>RELEASE SPOT</Text>
+          </Image>
+        </TouchableOpacity>
       );
-    } else if (route.id === 'join') {
+    }
+    else {
       return (
-        <Join
-          navigator={navigator}
-          changeLogin={this.handleLogin}
-          />
-      )
-    } else {
-      return (
-        <SignIn
-          navigator={navigator}
-          changeLogin={this.handleLogin}
-          />
+        <TouchableWithoutFeedback>
+          <Image style={styles.button} source={require('image!red_button')}>
+            <View>
+              <Text style={styles.buttonText}>TAKEN BY</Text>
+              <Text style={styles.buttonText}>{this.getOccupier()}</Text>
+            </View>
+          </Image>
+        </TouchableWithoutFeedback>
       );
     }
   },
 
-  renderPrivateNavigator() {
-    let initialRoute = { id: 'signIn', index: 0 };
-    if (this.props.userId) {
-      initialRoute.id = 'list';
+  renderButtons() {
+    if (this.getOccupier()) {
+      return (
+        <View style={styles.container}>{this.renderOccupied()}</View>
+      );
     }
+    else {
+      return (
+        <View style={styles.container}>{this.renderAvailable()}</View>
+      );
+    }
+  },
+
+  renderMain() {
+    return (
+      <TabBarIOS>
+        <TabBarIOS.Item
+          title="Logout"
+          icon={require('image!private')}
+          selected={true}
+          onPress={() => {
+            this.handleLogout({loggedIn: false});
+        }}>
+          {this.renderButtons()}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    )
+  },
+
+  renderAuth(route, navigator) {
+    if (route.id === 'join') {
+      return (
+        <Join navigator={navigator} changeLogin={this.handleLogin}/>
+      )
+    }
+    else {
+      return (
+        <SignIn navigator={navigator} changeLogin={this.handleLogin}/>
+      );
+    }
+  },
+
+  renderNavigatorScene(route, navigator) {
+    if (this.state.loggedIn) {
+      return this.renderMain();
+    }
+    else {
+      return this.renderAuth(route, navigator);
+    }
+  },
+
+  renderNavigator() {
+    let initialRoute = { id: 'signIn', index: 0 };
 
     return (
       <Navigator
-        ref="privateNavigator"
+        ref="navigator"
         initialRoute={initialRoute}
-        renderScene={this.renderPrivateScene}
+        renderScene={this.renderNavigatorScene}
         configureScene={(route) => {
           let config = Navigator.SceneConfigs.FloatFromBottom
           if (route.sceneConfig) {
@@ -141,33 +245,42 @@ let Layout = React.createClass({
     );
   },
 
-  render() {
+  renderConnecting() {
     return (
-      <TabBarIOS>
-        <TabBarIOS.Item
-          title="Public Lists"
-          icon={require('image!public')}
-          selected={this.state.selectedTab === 'public'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'public',
-            });
-          }}>
-          {this.renderPublicNavigator()}
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title="Private Lists"
-          icon={require('image!private')}
-          selected={this.state.selectedTab === 'private'}
-          onPress={() => {
-            this.setState({
-              selectedTab: 'private',
-            });
-          }}>
-          {this.renderPrivateNavigator()}
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <View>
+        <Text>Connecting</Text>
+      </View>
     )
+  },
+
+  render() {
+    if (this.state.connected) {
+      return this.renderNavigator();
+    }
+    else {
+      return this.renderConnecting()
+    }
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  button: {
+    width: 225,
+    height: 225,
+    backgroundColor: 'transparent'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+    top: 100
   }
 });
 
